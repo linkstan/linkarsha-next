00221de830608e8d42fae49eab5048a4290822fb
linkstan <linkarsha@gmail.com>
2026-02-28 01:45:11 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:'Arial'}}>
+      <h1>Linkarsha starting...</h1>
+      <p>Next.js connected successfully.</p>
+    </div>
+  )
+}
