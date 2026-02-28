47344b305b132e4fd89b7814aba771e689c7d719
linkstan <linkarsha@gmail.com>
2026-02-28 12:28:20 +0530

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://kgsunydafbgnivstjhcs.supabase.co"
-const supabaseAnonKey = "PASTE_YOUR_PUBLIC_ANON_KEY"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtnc3VueWRhZmJnbml2c3RqaGNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxNzk3MTEsImV4cCI6MjA4Nzc1NTcxMX0.oTvJcbbsnXPdSq0wc7rSyjJIezCZUxTz1Xe6FoC6ybs"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
