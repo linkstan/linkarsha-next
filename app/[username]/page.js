ebdadb8be8d488b82e5c3094c489ac0b5b72ff42
linkstan <linkarsha@gmail.com>
2026-03-01 12:15:09 +0530

    Update page.js

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://kgsunydafbgnivstjhcs.supabase.co",
-  "YOUR_ANON_KEY"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtnc3VueWRhZmJnbml2c3RqaGNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxNzk3MTEsImV4cCI6MjA4Nzc1NTcxMX0.oTvJcbbsnXPdSq0wc7rSyjJIezCZUxTz1Xe6FoC6ybs"
 );
 
 export default async function UserPage({ params }) {
