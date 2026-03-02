7bd354f5455bc492757bf8090bc146cfe8622bb3
linkstan <linkarsha@gmail.com>
2026-03-02 23:06:21 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,12 +1,5 @@
-"use client";
-
-import { Suspense } from "react";
 import SignupInner from "./signup-inner";
 
-export default function SignupPage(){
-  return (
-    <Suspense>
-      <SignupInner/>
-    </Suspense>
-  );
+export default function Page(){
+  return <SignupInner />;
 }
