eb5a646a7d9ed936d5bf7f6969eb31e8cb01d9cf
linkstan <linkarsha@gmail.com>
2026-03-03 03:05:50 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import SignupInner from "./signup-inner";
 
-export default function Page(){
-  return <SignupInner />;
+export default function Page() {
+  return (
+    <Suspense fallback={<div style={{color:"white",padding:40}}>Loading...</div>}>
+      <SignupInner />
+    </Suspense>
+  );
 }
