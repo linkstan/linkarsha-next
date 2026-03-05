9ca501dc6c6d0ed6f98f19f4402e1464655591cc
linkstan <linkarsha@gmail.com>
2026-03-06 03:35:53 +0530

    Update geo-map.js

--- app/dashboard/geo-map.js
+++ app/dashboard/geo-map.js
@@ -1,1 +1,58 @@
+"use client";
 
+export default function GeoMap({clickEvents=[]}){
+
+const countries = {};
+
+clickEvents.forEach(e=>{
+
+const country = e.country || "Unknown";
+
+countries[country] = (countries[country] || 0) + 1;
+
+});
+
+const sorted = Object.entries(countries)
+.sort((a,b)=>b[1]-a[1]);
+
+return(
+
+<div className="card">
+
+<h3>Audience Locations</h3>
+
+{sorted.map(([c,v])=>(
+
+<div key={c} className="row">
+
+<span>{c}</span>
+
+<span>{v}</span>
+
+</div>
+
+))}
+
+<style jsx>{`
+
+.card{
+background:#111;
+padding:25px;
+border-radius:16px;
+margin-bottom:30px;
+}
+
+.row{
+display:flex;
+justify-content:space-between;
+margin-top:10px;
+opacity:.85;
+}
+
+`}</style>
+
+</div>
+
+);
+
+}
