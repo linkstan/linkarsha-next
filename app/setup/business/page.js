fa656cd43a690e66d01e64811482af3a3e565051
linkstan <linkarsha@gmail.com>
2026-03-07 16:06:03 +0530

    Update page.js

--- app/setup/business/page.js
+++ app/setup/business/page.js
@@ -1,1 +1,23 @@
+"use client";
 
+export default function BusinessSetup(){
+
+return(
+
+<div style={{
+minHeight:"100vh",
+background:"#0b0b12",
+color:"white",
+display:"flex",
+alignItems:"center",
+justifyContent:"center",
+fontFamily:"-apple-system"
+}}>
+
+<h1>Business setup coming next</h1>
+
+</div>
+
+);
+
+}
