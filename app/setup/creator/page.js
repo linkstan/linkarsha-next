bb071b861b19aa5107f8103923db4535b8312d7c
linkstan <linkarsha@gmail.com>
2026-03-07 15:55:11 +0530

    Update page.js

--- app/setup/creator/page.js
+++ app/setup/creator/page.js
@@ -1,1 +1,23 @@
+"use client";
 
+export default function CreatorSetup(){
+
+return(
+
+<div style={{
+minHeight:"100vh",
+background:"#0b0b12",
+color:"white",
+display:"flex",
+alignItems:"center",
+justifyContent:"center",
+fontFamily:"-apple-system"
+}}>
+
+<h1>Creator setup coming next</h1>
+
+</div>
+
+);
+
+}
