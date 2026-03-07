1ec48ea71cd8d51a13e811948da20841b45e4c33
linkstan <linkarsha@gmail.com>
2026-03-07 16:05:24 +0530

    Update page.js

--- app/setup/personal/page.js
+++ app/setup/personal/page.js
@@ -1,1 +1,23 @@
+"use client";
 
+export default function PersonalSetup(){
+
+return(
+
+<div style={{
+minHeight:"100vh",
+background:"#0b0b12",
+color:"white",
+display:"flex",
+alignItems:"center",
+justifyContent:"center",
+fontFamily:"-apple-system"
+}}>
+
+<h1>Personal setup coming next</h1>
+
+</div>
+
+);
+
+}
