efb15d76677e9d9b625e809c534f09aa8789e4a6
linkstan <linkarsha@gmail.com>
2026-03-09 23:04:18 +0530

    Create page.js

---
+++ app/dashboard/referrals/page.js
@@ -0,0 +1,25 @@
+"use client";
+
+export default function Referrals(){
+
+return(
+
+<div style={{
+maxWidth:600
+}}>
+
+<h1>Referrals</h1>
+
+<p style={{marginTop:20,opacity:0.7}}>
+Something exciting is coming.
+</p>
+
+<p style={{marginTop:10}}>
+Invite friends soon and earn rewards.
+</p>
+
+</div>
+
+);
+
+}
