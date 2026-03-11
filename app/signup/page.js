0f13a3a04432fff6e51b8261aa6365ce002c3188
linkstan <linkarsha@gmail.com>
2026-03-11 22:48:44 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import SignupInner from "./signup-inner";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={<div style={{color:"white",padding:40}}>Loading...</div>}>
