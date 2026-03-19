a0d1f2748c7619beeae7ed6a3a8583873eea43b1
linkstan <linkarsha@gmail.com>
2026-03-19 17:43:45 +0530

    Update layout.js
    theme fix

--- app/layout.js
+++ app/layout.js
@@ -2,16 +2,16 @@ import "react-easy-crop/react-easy-crop.css";
 import "./globals.css";
 
 export const metadata = {
-  title: "Linkarsha",
-  description: "Creator economy OS"
+title: "Linkarsha",
+description: "Creator economy OS"
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{margin:0,fontFamily:"Arial"}}>
-        {children}
-      </body>
-    </html>
-  );
+return (
+<html lang="en">
+<body>
+{children}
+</body>
+</html>
+);
 }
