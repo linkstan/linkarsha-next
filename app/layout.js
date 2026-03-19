74c83bf169ef33c755df03c627027cdec8b0bb6c
linkstan <linkarsha@gmail.com>
2026-03-19 17:09:16 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{margin:0}}>
+      <body style={{margin:0,fontFamily:"Arial"}}>
         {children}
       </body>
     </html>
