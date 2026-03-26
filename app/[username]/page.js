21029169f286cc0cc7e3cc5991021541144237b5
linkstan <linkarsha@gmail.com>
2026-03-27 03:53:14 +0530

    Update page.js

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -9,7 +9,6 @@ process.env.NEXT_PUBLIC_SUPABASE_URL,
 process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
-export const revalidate = 60;
 export default function PublicProfile({ params }) {
 
 const [profile, setProfile] = useState(null);
