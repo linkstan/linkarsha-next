1794f628adcd7ef2a4c0ebd174eda458add5a19e
linkstan <linkarsha@gmail.com>
2026-04-15 08:44:11 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
 import "../styles/responsive.css";
 import "react-easy-crop/react-easy-crop.css";
 import "./globals.css";
+import { Playfair_Display, Dancing_Script, Inter } from "next/font/google";
 
+export const playfair = Playfair_Display({ subsets:["latin"] });
+export const dancing = Dancing_Script({ subsets:["latin"] });
+export const inter = Inter({ subsets:["latin"] });
 export const metadata = {
   title: "Linkarsha",
   description: "Creator economy OS"
