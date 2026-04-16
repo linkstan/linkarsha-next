42473f06c5ed32c2ec25025fd9f553f90a37e165
linkstan <linkarsha@gmail.com>
2026-04-16 14:58:12 +0530

    Update HeroHeader.js

--- components/HeroHeader.js
+++ components/HeroHeader.js
@@ -75,10 +75,10 @@ textAlign:"center"
 <div
 style={{
 position:"absolute",
-bottom:-50,
+bottom:-40,
 left:"50%",
 transform:"translateX(-50%)",
-width:"40%",
+width:"30%",
 height:120,
 background:theme.background || "#e9ded9",
 borderRadius:"50%"
