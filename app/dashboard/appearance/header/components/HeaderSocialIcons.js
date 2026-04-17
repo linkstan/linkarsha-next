c313cc4fa4724e2eeee29161fa1884e8eb56f1e1
linkstan <linkarsha@gmail.com>
2026-04-17 16:29:23 +0000

    Create HeaderSocialIcons.js

---
+++ app/dashboard/appearance/header/components/HeaderSocialIcons.js
@@ -0,0 +1,59 @@
+"use client";
+
+export default function HeaderSocialIcons({
+section,
+settings,
+updateSetting,
+themeFeatures
+}){
+
+if(!themeFeatures.socialIcons) return null;
+
+return(
+
+<div style={section}>
+
+<h3>Social Icons</h3>
+
+<label>
+<input
+type="checkbox"
+checked={settings.showSocialIcons}
+onChange={(e)=>updateSetting("showSocialIcons",e.target.checked)}
+/>
+ Show Social Icons
+</label>
+
+{settings.showSocialIcons && (
+
+<div style={{marginTop:12}}>
+
+<div style={{fontSize:14,marginBottom:6}}>Show in:</div>
+
+<label style={{marginRight:12}}>
+<input
+type="radio"
+checked={settings.socialPosition==="header"}
+onChange={()=>updateSetting("socialPosition","header")}
+/>
+ Header
+</label>
+
+<label>
+<input
+type="radio"
+checked={settings.socialPosition==="bottom"}
+onChange={()=>updateSetting("socialPosition","bottom")}
+/>
+ Bottom of Links
+</label>
+
+</div>
+
+)}
+
+</div>
+
+);
+
+}
