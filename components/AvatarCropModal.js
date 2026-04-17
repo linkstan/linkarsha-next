ca35aa0ce3d8705299d9125fdd8f75ab2f728dff
linkstan <linkarsha@gmail.com>
2026-04-18 01:42:59 +0530

    Create AvatarCropModal.js

---
+++ components/AvatarCropModal.js
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import Cropper from "react-easy-crop";
+
+export default function AvatarCropModal({
+image,
+shape,
+onCancel,
+onComplete
+}){
+
+const [crop,setCrop]=useState({x:0,y:0});
+const [zoom,setZoom]=useState(1);
+const [area,setArea]=useState(null);
+
+return(
+
+<div style={{
+position:"fixed",
+top:0,
+left:0,
+right:0,
+bottom:0,
+background:"rgba(0,0,0,0.7)",
+display:"flex",
+alignItems:"center",
+justifyContent:"center",
+zIndex:9999
+}}>
+
+<div style={{
+width:320,
+height:320,
+background:"#111",
+position:"relative"
+}}>
+
+<Cropper
+image={image}
+crop={crop}
+zoom={zoom}
+aspect={1}
+cropShape={shape}
+onCropChange={setCrop}
+onZoomChange={setZoom}
+onCropComplete={(c,a)=>setArea(a)}
+/>
+
+</div>
+
+<div style={{marginTop:20,display:"flex",gap:10}}>
+
+<button onClick={onCancel}>Cancel</button>
+
+<button onClick={()=>onComplete(area,zoom)}>
+Save
+</button>
+
+</div>
+
+</div>
+
+);
+
+}
