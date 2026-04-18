f4634834caf1edc9470a1702fb2c91a38c3fdb3c
linkstan <linkarsha@gmail.com>
2026-04-18 15:07:55 +0530

    Update cropImage.js

--- app/lib/cropImage.js
+++ app/lib/cropImage.js
@@ -1,4 +1,4 @@
-export default function getCroppedImg(imageSrc, crop, zoom) {
+export default function getCroppedImg(imageSrc, crop) {
 return new Promise((resolve) => {
 
 const image = new Image();
@@ -9,10 +9,12 @@ image.onload = () => {
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 
+/* create a PERFECT square image */
+
 const size = 1200;
 
 canvas.width = size;
-canvas.height = size * 0.35;
+canvas.height = size;
 
 ctx.drawImage(
 image,
@@ -22,13 +24,13 @@ crop.width,
 crop.height,
 0,
 0,
-canvas.width,
-canvas.height
+size,
+size
 );
 
 canvas.toBlob((blob) => {
 resolve(blob);
-}, "image/jpeg", 0.9);
+}, "image/jpeg", 0.95);
 
 };
 
