dfdc9c1d198441afa3f3b23522689cffffa2284f
linkstan <linkarsha@gmail.com>
2026-04-18 19:33:03 +0530

    Update HeroHeader.js

--- components/HeroHeader.js
+++ components/HeroHeader.js
@@ -44,18 +44,16 @@ overflow:"hidden"
 
 {heroImage && (
 
-<div
+<img
+src={heroImage}
 style={{
 position:"absolute",
 top:0,
 left:0,
-right:0,
-bottom:0,
-backgroundImage:`url(${heroImage})`,
-backgroundSize:"contain",
-backgroundPosition: heroPosition,
-backgroundAttachment: "initial",
-backgroundRepeat:"no-repeat",
+width:"100%",
+height:"100%",
+objectFit:"cover",
+objectPosition: heroPosition,
 opacity:heroOpacity,
 transition:"opacity .3s ease"
 }}
