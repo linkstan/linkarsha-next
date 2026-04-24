cb42b85f4d06ac9e702c6a3dcd0f2f37d9485628
linkstan <linkarsha@gmail.com>
2026-04-25 03:24:34 +0530

    Update BlueprintTheme.js

--- components/themes/BlueprintTheme.js
+++ components/themes/BlueprintTheme.js
@@ -30,11 +30,88 @@ color:"#f1e2d6"
 style={{
 width:"100%",
 background:"#e9e1d8",
-padding:"50px 20px 110px",
-textAlign:"center"
+padding:"50px 20px 80px",
+textAlign:"center",
+position:"relative"
 }}
 >
 
+{/* PROFILE IMAGE */}
+
+<div
+style={{
+width:80,
+height:80,
+borderRadius:"50%",
+margin:"0 auto 10px",
+overflow:"hidden"
+}}
+>
+
+<img
+src={profile.avatar || ""}
+style={{
+width:"100%",
+height:"100%",
+objectFit:"cover"
+}}
+/>
+
+</div>
+
+
+{/* NAME */}
+
+<div
+style={{
+fontSize:26,
+fontWeight:600,
+letterSpacing:1,
+color:"#2f5668"
+}}
+>
+{profile.display_name || profile.username}
+</div>
+
+
+{/* SUBTITLE */}
+
+{profile.subtitle && (
+
+<div
+style={{
+fontSize:14,
+opacity:.7,
+marginTop:4
+}}
+>
+{profile.subtitle}
+</div>
+
+)}
+
+
+{/* WAVE DIVIDER */}
+
+<svg
+viewBox="0 0 1440 120"
+style={{
+position:"absolute",
+bottom:-1,
+left:0,
+width:"100%",
+height:120
+}}
+>
+
+<path
+fill="#2f5668"
+d="M0,64L80,74.7C160,85,320,107,480,101.3C640,96,800,64,960,53.3C1120,43,1280,53,1360,58.7L1440,64L1440,160L1360,160C1280,160,1120,160,960,160C800,160,640,160,480,160C320,160,160,160,80,160L0,160Z"
+/>
+
+</svg>
+
+</div>
 {/* LOGO / PROFILE */}
 
 <div
