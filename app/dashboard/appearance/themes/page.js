f8d477642d4b386767c765c58c5994d8739dbf51
linkstan <linkarsha@gmail.com>
2026-04-27 13:18:53 +0530

    Update page.js

--- app/dashboard/appearance/themes/page.js
+++ app/dashboard/appearance/themes/page.js
@@ -21,7 +21,7 @@ const themes=[
 
 {name:"archway",display:"Archway",category:"Professional",bg:"#f3efe9"},
   
-{name:"blueprint",display:"Blueprint",category:"Business",bg:"#2f5668"}
+{name:"blueprint",display:"Blueprint",category:"Business",bg:"#2f5668"},
   
 {name:"solstice",display:"Solstice",category:"Business",bg:"linear-gradient(#7a2c00,#4d2200)"}
   
