627e5bc285d1557f3c0004cd9eaf75c7c596f606
linkstan <linkarsha@gmail.com>
2026-05-16 02:50:49 +0530

    Update layoutPresets.js

--- app/lib/layoutPresets.js
+++ app/lib/layoutPresets.js
@@ -1,29 +1,89 @@
-export const layoutPresets = {
+const layoutPresets = {
+
+centered:{
+
+nameSize:56,
+usernameSize:18,
+bioSize:15,
+
+headerTopSpacing:42,
+
+sectionSpacing:54,
+
+contentWidth:420,
+
+lineHeight:1.5
+
+},
 
 hero:{
-nameSize:88,
-sectionSpacing:90,
-contentWidth:760,
-headerTopSpacing:220
+
+nameSize:108,
+usernameSize:24,
+bioSize:20,
+
+headerTopSpacing:240,
+
+sectionSpacing:120,
+
+contentWidth:920,
+
+lineHeight:1.2,
+
+letterSpacing:-0.06
+
 },
 
 editorial:{
-align:"left",
-contentWidth:520,
-lineHeight:1.9,
-letterSpacing:-0.02
-},
 
-card:{
-contentWidth:560
+nameSize:82,
+usernameSize:20,
+bioSize:18,
+
+headerTopSpacing:80,
+
+sectionSpacing:72,
+
+contentWidth:760,
+
+lineHeight:1.8,
+
+letterSpacing:-0.05
+
 },
 
 split:{
-contentWidth:1200
+
+nameSize:92,
+usernameSize:20,
+bioSize:17,
+
+headerTopSpacing:20,
+
+sectionSpacing:70,
+
+contentWidth:1200,
+
+lineHeight:1.45
+
 },
 
-centered:{
-contentWidth:420
+card:{
+
+nameSize:64,
+usernameSize:18,
+bioSize:16,
+
+headerTopSpacing:70,
+
+sectionSpacing:60,
+
+contentWidth:540,
+
+lineHeight:1.5
+
 }
 
 };
+
+export default layoutPresets;
