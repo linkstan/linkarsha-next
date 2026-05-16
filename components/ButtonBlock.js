0b587e03cbab7797d7907d0b5c33a6c4baaa05a2
linkstan <linkarsha@gmail.com>
2026-05-17 02:27:16 +0530

    Update ButtonBlock.js

--- components/ButtonBlock.js
+++ components/ButtonBlock.js
@@ -669,5 +669,31 @@ fontWeight:500
 </a>
 
 );
+<style jsx global>{`
 
+@keyframes buttonEntrance {
+
+0%{
+
+opacity:0;
+
+transform:
+translateY(18px)
+scale(.98);
+
+}
+
+100%{
+
+opacity:1;
+
+transform:
+translateY(0px)
+scale(1);
+
+}
+
+}
+
+`}</style>
 }
