8db9e019da53fecbe5c38dda872c5125594ce2c2
linkstan <linkarsha@gmail.com>
2026-05-17 15:18:13 +0530

    Create ButtonMotion.js

---
+++ components/renderers/blocks/ButtonMotion.js
@@ -0,0 +1,29 @@
+"use client";
+
+export const buttonEntranceKeyframes = `
+
+@keyframes buttonEntrance {
+
+0%{
+
+opacity:0;
+
+transform:
+translateY(18px)
+scale(.98);
+
+}
+
+100%{
+
+opacity:1;
+
+transform:
+translateY(0px)
+scale(1);
+
+}
+
+}
+
+`;
