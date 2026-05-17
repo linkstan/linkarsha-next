b73e874c8d7ca15ffa2f23e5e7f3704e1d18a405
linkstan <linkarsha@gmail.com>
2026-05-17 14:59:09 +0530

    Create ButtonBlockRenderer.js

---
+++ components/renderers/blocks/ButtonBlockRenderer.js
@@ -0,0 +1,31 @@
+"use client";
+
+import ButtonBlock
+from "../../ButtonBlock";
+
+export default function ButtonBlockRenderer({
+
+block,
+theme,
+appearance,
+index
+
+}){
+
+return(
+
+<ButtonBlock
+
+block={block}
+
+theme={theme}
+
+appearance={appearance}
+
+index={index}
+
+/>
+
+);
+
+}
