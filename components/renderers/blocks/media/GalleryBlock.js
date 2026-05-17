6fd62add40164e032a9eb0348f5a0bc809b161d8
linkstan <linkarsha@gmail.com>
2026-05-17 15:23:31 +0530

    Update GalleryBlock.js

--- components/renderers/blocks/media/GalleryBlock.js
+++ components/renderers/blocks/media/GalleryBlock.js
@@ -1,1 +1,65 @@
+"use client";
 
+export default function GalleryBlock({
+
+block
+
+}){
+
+const images =
+block?.data_json?.images || [];
+
+if(images.length === 0){
+
+return null;
+
+}
+
+return(
+
+<div
+style={{
+
+display:"grid",
+
+gridTemplateColumns:
+"repeat(2,minmax(0,1fr))",
+
+gap:14,
+
+width:"100%"
+
+}}
+>
+
+{images.map((image,index)=>(
+
+<img
+key={index}
+
+src={image}
+
+alt="Gallery"
+
+style={{
+
+width:"100%",
+
+aspectRatio:"1 / 1",
+
+objectFit:"cover",
+
+borderRadius:22,
+
+display:"block"
+
+}}
+/>
+
+))}
+
+</div>
+
+);
+
+}
