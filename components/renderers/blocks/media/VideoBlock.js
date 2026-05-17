3c03fbe868243dfe6160055d13a5c73778e81573
linkstan <linkarsha@gmail.com>
2026-05-17 15:22:39 +0530

    Update VideoBlock.js

--- components/renderers/blocks/media/VideoBlock.js
+++ components/renderers/blocks/media/VideoBlock.js
@@ -1,1 +1,59 @@
+"use client";
 
+export default function VideoBlock({
+
+block
+
+}){
+
+const videoUrl =
+block?.data_json?.videoUrl;
+
+if(!videoUrl){
+
+return null;
+
+}
+
+return(
+
+<div
+style={{
+
+width:"100%",
+
+borderRadius:28,
+
+overflow:"hidden",
+
+position:"relative",
+
+aspectRatio:"16 / 9",
+
+background:"#000000"
+
+}}
+>
+
+<iframe
+src={videoUrl}
+allowFullScreen
+
+style={{
+
+width:"100%",
+height:"100%",
+
+border:"none",
+
+position:"absolute",
+inset:0
+
+}}
+/>
+
+</div>
+
+);
+
+}
