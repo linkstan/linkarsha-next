6f81e571a653104faa8c14267f66ca9972bfc19a
linkstan <linkarsha@gmail.com>
2026-05-17 13:38:43 +0530

    Create MotionEffects.js

---
+++ components/renderers/MotionEffects.js
@@ -0,0 +1,35 @@
+export const floatingGlowKeyframes = `
+@keyframes floatingGlow {
+
+0%{
+transform:
+translate(0px,0px)
+scale(1);
+}
+
+25%{
+transform:
+translate(12px,-8px)
+scale(1.04);
+}
+
+50%{
+transform:
+translate(-6px,12px)
+scale(1.02);
+}
+
+75%{
+transform:
+translate(10px,6px)
+scale(1.05);
+}
+
+100%{
+transform:
+translate(0px,0px)
+scale(1);
+}
+
+}
+`;
