72263ebf1d0d958d991f32b78616000d3a82813e
linkstan <linkarsha@gmail.com>
2026-05-17 13:40:58 +0530

    Update ThemeRenderer.js

--- components/ThemeRenderer.js
+++ components/ThemeRenderer.js
@@ -20,6 +20,10 @@ import { layoutPresets }
 from "../app/lib/layoutPresets";
 
 import {
+floatingGlowKeyframes
+} from "./renderers/MotionEffects";
+
+import {
 useEffect,
 useState
 } from "react";
@@ -1597,41 +1601,7 @@ appearance={finalAppearance}
 </div>
 
 <style jsx global>{`
-
-@keyframes floatingGlow {
-
-0%{
-transform:
-translate(0px,0px)
-scale(1);
-}
-
-25%{
-transform:
-translate(12px,-8px)
-scale(1.04);
-}
-
-50%{
-transform:
-translate(-6px,12px)
-scale(1.02);
-}
-
-75%{
-transform:
-translate(10px,6px)
-scale(1.05);
-}
-
-100%{
-transform:
-translate(0px,0px)
-scale(1);
-}
-
-}
-
+${floatingGlowKeyframes}
 `}</style>
 
 </>
