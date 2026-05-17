cadc510b6a519f919d34c8c602b279a866341abf
linkstan <linkarsha@gmail.com>
2026-05-17 15:23:09 +0530

    Update MusicBlock.js

--- components/renderers/blocks/media/MusicBlock.js
+++ components/renderers/blocks/media/MusicBlock.js
@@ -1,1 +1,53 @@
+"use client";
 
+export default function MusicBlock({
+
+block
+
+}){
+
+const embedUrl =
+block?.data_json?.embedUrl;
+
+if(!embedUrl){
+
+return null;
+
+}
+
+return(
+
+<div
+style={{
+
+width:"100%",
+
+borderRadius:24,
+
+overflow:"hidden",
+
+background:"#111111"
+
+}}
+>
+
+<iframe
+src={embedUrl}
+
+allow="autoplay; clipboard-write; encrypted-media; fullscreen"
+
+style={{
+
+width:"100%",
+height:160,
+
+border:"none"
+
+}}
+/>
+
+</div>
+
+);
+
+}
