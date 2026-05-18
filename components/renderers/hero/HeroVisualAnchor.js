aca8b361badc6c1dff37727af2cfc416ef244878
linkstan <linkarsha@gmail.com>
2026-05-19 04:09:24 +0530

    Update HeroVisualAnchor.js

--- components/renderers/hero/HeroVisualAnchor.js
+++ components/renderers/hero/HeroVisualAnchor.js
@@ -1,1 +1,47 @@
+"use client";
 
+export default function HeroVisualAnchor({
+
+anchor
+
+}){
+
+if(!anchor?.show){
+
+return null;
+
+}
+
+return(
+
+<div
+style={{
+
+position:"absolute",
+
+width:anchor.width,
+height:anchor.height,
+
+left:anchor.x,
+top:anchor.y,
+
+transform:"translateX(-50%)",
+
+borderRadius:"50%",
+
+background:anchor.gradient,
+
+filter:`blur(${anchor.blur}px)`,
+
+opacity:anchor.opacity,
+
+pointerEvents:"none",
+
+zIndex:0
+
+}}
+/>
+
+);
+
+}
