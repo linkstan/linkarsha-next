1403cb6924cba7641965cd97fdbb850aacc2b3b6
linkstan <linkarsha@gmail.com>
2026-05-19 04:08:57 +0530

    Update CinematicHeroContainer.js

--- components/renderers/hero/CinematicHeroContainer.js
+++ components/renderers/hero/CinematicHeroContainer.js
@@ -1,1 +1,51 @@
+"use client";
 
+export default function CinematicHeroContainer({
+
+children,
+cinematic
+
+}){
+
+return(
+
+<div
+style={{
+
+position:"relative",
+
+width:"100%",
+
+overflow:"hidden",
+
+display:"flex",
+justifyContent:"center",
+
+paddingLeft:24,
+paddingRight:24
+
+}}
+>
+
+<div
+style={{
+
+position:"relative",
+
+width:"100%",
+
+maxWidth:
+cinematic?.heroWidth || "100%"
+
+}}
+>
+
+{children}
+
+</div>
+
+</div>
+
+);
+
+}
