62268b0ca2d5a3ba63bef250b1ccb025552f2911
linkstan <linkarsha@gmail.com>
2026-05-20 14:31:24 +0530

    Update CreatorIdentityStrip.js

--- components/renderers/trust/CreatorIdentityStrip.js
+++ components/renderers/trust/CreatorIdentityStrip.js
@@ -1,44 +1,7 @@
-export default function CreatorIdentityStrip({
-signals
-}){
+"use client";
 
-return(
+export default function CreatorIdentityStrip(){
 
-<div
-style={{
-
-display:"flex",
-alignItems:"center",
-
-gap:10,
-
-marginBottom:20,
-
-fontSize:12,
-
-letterSpacing:".12em",
-
-textTransform:"uppercase",
-
-opacity:.62
-
-}}
->
-
-<div>
-Premium Creator
-</div>
-
-<div>
-• 
-</div>
-
-<div>
-{signals?.availability}
-</div>
-
-</div>
-
-);
+return null;
 
 }
